bfc41858564444b6495323d3a852b625b1bdfacb
jasmine <109494334+jasminex21@users.noreply.github.com>
2023-01-02 13:05:29 -0600

    Update server.R

--- server.R
+++ server.R
@@ -133,7 +133,7 @@ server = function(input, output) {
                  color = rep_len(cloudPalette()[2:length(cloudPalette())], 
                                  length.out = nrow(createWordCount())), 
                  backgroundColor = cloudPalette()[1], 
-                 size = 0.85)
+                 size = 0.65)
     })
   
   ## Panel 4 ##
