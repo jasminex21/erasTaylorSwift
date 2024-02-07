a92b1ab940faf9c137a281a5e4fc9b75f81441f0
jasmine <109494334+jasminex21@users.noreply.github.com>
2024-02-07 11:25:47 -0600

    dev: updating lyrics to include SN TV and 1989 TV and removing (all) old versions

--- lyrics.R
+++ lyrics.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(taylor)
 
 # Original dataframe created by taylorData.R
-taylor = read_csv("taylorRevised.csv")
+taylor = read_csv("taylorRevised_2024.csv")
 
 # Making slight changes to track names in both dataframes such
 # that the track names match perfectly
@@ -12,6 +12,10 @@ taylor_all_songs$track_name = gsub("\\[", "(", taylor_all_songs$track_name)
 taylor_all_songs$track_name = gsub("\\]", ")", taylor_all_songs$track_name)
 taylor$track_name = gsub("<U\\+2019>", "'", taylor$track_name)
 taylor$track_name = gsub("<U\\+2018>", "'", taylor$track_name)
+taylor$track_name = gsub('\"', '', taylor$track_name)
+taylor_all_songs$track_name = gsub('\"', '', taylor_all_songs$track_name)
+taylor$track_name = gsub("’", "'", taylor$track_name)
+taylor$track_name = gsub("‘", "'", taylor$track_name)
 taylor$track_name = str_trim(gsub("\\s\\(feat[^()]*\\)", "", taylor$track_name))
 taylor$track_name = str_trim(gsub("- bonus track", "", taylor$track_name))
 
@@ -19,6 +23,7 @@ taylor$track_name[taylor$track_name == "A Place in this World"] = "A Place In Th
 taylor$track_name[taylor$track_name == "Tied Together with a Smile"] = "Tied Together With A Smile"
 taylor$track_name[taylor$track_name == "I Knew You Were Trouble."] = "I Knew You Were Trouble"
 taylor$track_name[taylor$track_name == "Superstar (Taylor's Version)"] = "SuperStar (Taylor's Version)" 
+taylor$track_name[taylor$track_name == "When Emma Falls in Love (Taylor's Version) (From The Vault)"] = "When Emma Falls In Love (Taylor's Version) (From The Vault)" 
 
 taylor_all_songs$album_name[taylor_all_songs$track_name == "I'm Only Me When I'm With You"] = "Taylor Swift"
 
@@ -36,4 +41,4 @@ allLyrics = taylor_all_songs %>%
   unnest(lyrics) %>%
   select(-(element_artist))
 
-write.csv(allLyrics, "allTaylorLyrics.csv")
+write.csv(allLyrics, "allTaylorLyrics_2024.csv")
